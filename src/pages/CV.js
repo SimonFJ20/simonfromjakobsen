fdb0bac90ff68dd2ca3d1d8bd32754b5c3803c2a
Simon From <simonfromjakobsen@gmail.com>
2021-10-17 15:56:24 +0200

    added cv

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -9,17 +9,26 @@ export default function CV(props) {
                     CV
                 </h1>
 
-                <h2>Denne side er under konstruktion.</h2>
+                <h2><b>Hvem er jeg</b></h2>
         
-            <hr />
-
-            <h1>Lidt om mig</h1>
 
             <h2>
                 Jeg er 17 år gammel. Jeg har bestået folkeskole eksamen og GF1 på EUX. Jeg er i gang med GF2 på EUX<br/>
                 som Datatekniker m. s. i. programmering. Jeg har stor interresse indenfor IT og programmering. Jeg<br/>
                 har programmeret 6 år. 
             </h2>
+
+            <br/>
+            <br/>
+
+            <div id="cv-buttons">
+                <a href="/static/simonfromjakobsen_cv_teknisk.pdf" target="_blank">
+                    Mit CV (Teknisk)
+                </a>
+                <a href="/static/simonfromjakobsen_cv.pdf" target="_blank">
+                    Mit CV
+                </a>
+            </div>
 
             <hr />
             <h1>
